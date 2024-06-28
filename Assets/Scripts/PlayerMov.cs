8726652f7a4a33f92e83251e40874dc60d2ab6c9
Albertengo <145947798+Albertengo@users.noreply.github.com>
2024-06-28 18:09:47 -0300

    movimiento funciona

--- Assets/Scripts/PlayerMov.cs
+++ Assets/Scripts/PlayerMov.cs
@@ -20,25 +20,25 @@ public class PlayerMov : MonoBehaviour
 
     private void Movement()
     {
-        if (isActive == false) //mientras estes en el piso, se podra saltar, es para evitar volar
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                rb.AddForce(Vector3.up * speed);
-                isActive = true;
-            }
-            if (Input.GetKeyUp(KeyCode.Space))
-                isActive = false;
-        }
-            //Debug.Log("Estas en el piso");
+        //if (isActive == false) //mientras estes en el piso, se podra saltar, es para evitar volar
+        //{
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        rb.AddForce(Vector3.up * speed);
+        //        isActive = true;
+        //    }
+        //    if (Input.GetKeyUp(KeyCode.Space))
+        //        isActive = false;
+        //}
+        //Debug.Log("Estas en el piso");
 
         //float horizontalInput = Input.GetAxis("Horizontal");
-        //float verticalInput = Input.GetAxis("Vertical");
-        //Vector3 movement = new Vector3(horizontalInput, 0f, 0f); // verticalInput);
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(0f, 0f, verticalInput); // verticalInput);
 
-        //movement.Normalize();
+        movement.Normalize();
 
-        //transform.Translate(movement * Time.deltaTime * speed); //para moverse a los cpstados segun el player
+        transform.Translate(movement * Time.deltaTime * speed); //para moverse a los cpstados segun el player
         //transform.Translate(Vector3.forward * Time.deltaTime * speedworld, Space.World); //constantemente se mueve al frente
     }
 }
