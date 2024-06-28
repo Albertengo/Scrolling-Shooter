09704d6647b16da4923f473eb51a64c98e0ba186
Albertengo <145947798+Albertengo@users.noreply.github.com>
2024-06-28 19:33:36 -0300

    funciona disparo + empezando menú inicio

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject BalaPrefab;
+    public Transform FirePoint;
     void Start()
     {
         
@@ -13,6 +14,14 @@ public class PlayerShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            disparar();
+        }
+    }
+
+    void disparar()
+    {
+        Instantiate(BalaPrefab, FirePoint.position, FirePoint.rotation);
     }
 }
